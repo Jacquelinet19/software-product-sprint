d75253c282efdbb40b7390913b7b1bc22148526b
Jacqueline <jacquelinetapia8@gmail.com>
2021-03-23 21:45:05 +0000

    Week 2 Project 2

--- portfolio/src/main/webapp/hellojackiescript.js
+++
@@ -1,8 +0,0 @@
-/** Fetches the current date from the server and adds it to the page. */
-async function hellojackie() {
-  const responseFromServer = await fetch('/hellojackie');
-  const textFromResponse = await responseFromServer.text();
-
-  const helloContainer = document.getElementById('hello-jackie-container');
-  helloContainer.innerText = textFromResponse;
-}
