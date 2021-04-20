9b9a605ba91c007a94fde11055daa49e85bad235
Jacqueline <jacquelinetapia8@gmail.com>
2021-04-20 14:12:29 +0000

    Add java class for object contact

---
+++ portfolio/src/main/java/com/google/sps/servlets/contact.java
@@ -0,0 +1,11 @@
+public final class Contact{
+    private final String name;
+    private final String email;
+    private final String reason;
+
+    public Contact(String name, String email, String reason){
+        this.name=name;
+        this.email=email;
+        this.reason=reason;
+    }
+}
